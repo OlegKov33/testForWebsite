6c63979de092f10bff7325ed4a57f60bed88b13b
OlegKov33 <91954137+OlegKov33@users.noreply.github.com>
2022-04-12 16:01:42 +0100

    Add files via upload

---
+++ final version/Final/Assets/JS/MOHAMED.js
@@ -0,0 +1,11 @@
+function handle_form_submission()
+{
+  alert('Thank you for your membership application. We welcome your interest in becoming a member of the RGU Classical music society');
+  return false; //do not submit the form
+}
+
+function handle_form_subscribe()
+{
+  alert('Thank you for subscribing to our newsletter');
+  return false; //do not submit the form
+}
---
+++ final version/Final/Assets/JS/MOHAMED2.js
@@ -0,0 +1,5 @@
+function handle_form_submission()
+{
+  alert('Thank you for your inquiry. we will respond as soon as possible');
+  return false; //do not submit the form
+}
